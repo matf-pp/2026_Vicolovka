75a608ba0ac4a17b2ee5e092c3ecc7fbbf0b9b37
tanev igor <tanevigor@gmail.com>
2026-06-11 18:36:55 +0200

    mrzim poruke

--- Vicolovka/mapGen.lua
+++ Vicolovka/mapGen.lua
@@ -39,19 +39,19 @@ local function rotate(shape)
     return rotate
 end
 
-local function put_start(map)
+local function put_start(grid)
     
     local start
     repeat
-        start = math.random(1, #map)
+        start = math.random(1, #grid)
     until start ~= 4 and start ~= 5
 
-    map[start][1] = 0
+    grid[start][1] = 0
 
     return start
 end
 
-local function check_shape(x, y, shape, map)
+local function check_shape(x, y, shape, grid)
 
     for i = 1, #shape do
         local dx = shape[i][1]
@@ -63,15 +63,15 @@ local function check_shape(x, y, shape, map)
         --dead_end_check[1] = dead_end_check[1] + dx
         --dead_end_check[2] = dead_end_check[2] + dy
 
-        if target_y < 1 or target_y > #map or target_x < 1 or target_x > #map[1] then
+        if target_y < 1 or target_y > #grid or target_x < 1 or target_x > #grid[1] then
             return false
         elseif y == 7 and dy == 1 then
             return false
         elseif y == 8 and dy == -1 then
             return false
-        elseif map[target_y][target_x] == -2 then
+        elseif grid[target_y][target_x] == -2 then
             return false
-        elseif map[target_y][target_x] > 0 then
+        elseif grid[target_y][target_x] > 0 then
             return false
         end
     end 
@@ -79,18 +79,18 @@ local function check_shape(x, y, shape, map)
     return true
 end
 
-local function get_valid_shapes(x, y, map)
+local function get_valid_shapes(x, y, grid)
     local check_shape_shapes = {}
 
     for _, shape in ipairs(Shapes) do
         local r_sh = shape
         if shape == O_shape then
-            if check_shape(x, y, r_sh, map) then
+            if check_shape(x, y, r_sh, grid) then
                 table.insert(check_shape_shapes, r_sh)
             end
         elseif shape == I_shape then
             for i = 1, 2 do
-                if check_shape(x, y, r_sh, map) then
+                if check_shape(x, y, r_sh, grid) then
                     table.insert(check_shape_shapes, r_sh)
                 end
 
@@ -98,7 +98,7 @@ local function get_valid_shapes(x, y, map)
             end
         else
            for i = 1, 4 do
-                if check_shape(x, y, r_sh, map) then
+                if check_shape(x, y, r_sh, grid) then
                     table.insert(check_shape_shapes, r_sh)
                 end
 
@@ -110,70 +110,70 @@ local function get_valid_shapes(x, y, map)
     return check_shape_shapes
 end
 
-local function next_pos(start_y, map)
-    for y = start_y, #map do
-        if map[y][1] == -1 then return 1 , y end
+local function next_pos(start_y, grid)
+    for y = start_y, #grid do
+        if grid[y][1] == -1 then return 1 , y end
     end
 
-    for x = 1, #map[1] do
-        for y = 1, #map do
-            if map[y][x] == -1 then return x, y end
+    for x = 1, #grid[1] do
+        for y = 1, #grid do
+            if grid[y][x] == -1 then return x, y end
         end
     end
 
     return -1, -1
 end
 
-local function put_shape(x, y, shape, map, num)
+local function put_shape(x, y, shape, grid, num)
 
     for i = 1, #shape do
         local dx = shape[i][1]
         local dy = shape[i][2]
 
-        map[y + dy][x + dx] = num
+        grid[y + dy][x + dx] = num
     end
     
     num = num + 1
     return num
 end
 
-local function tetris_gen(map)
+local function tetris_gen(grid)
     local tile_num = 0
-    local start = put_start(map)
+    local start = put_start(grid)
 
     local y = start
     local x = 1 
-    local max_steps = #map * #map[1]
+    local max_steps = #grid * #grid[1]
 
     for step = 1, max_steps do
-        local valid_shapes = get_valid_shapes(x, y, map)
+        local valid_shapes = get_valid_shapes(x, y, grid)
 
         if #valid_shapes > 0 then
             local chosen_shape = valid_shapes[math.random(#valid_shapes)]
-            tile_num = put_shape(x, y, chosen_shape, map, tile_num)
-        elseif map[y][x] == -1 then
-            map[y][x] = tile_num
+            tile_num = put_shape(x, y, chosen_shape, grid, tile_num)
+        elseif grid[y][x] == -1 then
+            grid[y][x] = tile_num
             tile_num = tile_num + 1
         end
 
-        x, y = next_pos(y, map)
+        x, y = next_pos(y, grid)
         if x == -1 then break end
     end
 
 end
 
-local function grid_expand(map)
-    local row = #map
-    local col = #map[1]
+local function grid_expand(grid)
+    local row = #grid
+    local col = #grid[1]
 
     local ex_row = (row * 2) - 1
     local ex_col = (col * 2) - 1
-    local ex_map = {}
+    local ex_grid = {}
 
     for i = 1, ex_row do
-        ex_map[i] = {}
+        ex_grid[i] = {}
         for j = 1, ex_col do
-            ex_map[i][j] = -1
+            ex_grid[i][j] = -1
         end
     end
 
@@ -181,32 +181,32 @@ local function grid_expand(map)
         for x = 1, col do
             local ex = (x * 2) - 1
             local ey = (y * 2) - 1
-            ex_map[ey][ex] = map[y][x]
+            ex_grid[ey][ex] = grid[y][x]
         end
     end
 
-    return ex_map
+    return ex_grid
 end
 
-local function create_roads(map)
+local function create_roads(grid)
     -- TODO uvesti dodatni pregled mrtvih puteva
-    for y = 1, #map do
-        for x = 1, #map[1] do
+    for y = 1, #grid do
+        for x = 1, #grid[1] do
             if y >= 7 and y <= 9 and x >= 1 and x <= 3 then
-                map[y][x] = -2
+                grid[y][x] = -2
             elseif y == 14 and x == 1 then
-                map[y][x] = -1
+                grid[y][x] = -1
 
-            elseif map[y][x] == -1 then
+            elseif grid[y][x] == -1 then
 
-                if x > 1 and x < #map[1] then
-                    if map[y][x-1] > -1 and map[y][x+1] > -1 and map[y][x-1] == map[y][x+1] then
-                        map[y][x] = map[y][x-1]
+                if x > 1 and x < #grid[1] then
+                    if grid[y][x-1] > -1 and grid[y][x+1] > -1 and grid[y][x-1] == grid[y][x+1] then
+                        grid[y][x] = grid[y][x-1]
                     end
                 end
-                if y > 1 and y < #map then
-                    if map[y-1][x] > -1 and map[y+1][x] > -1 and map[y-1][x] == map[y+1][x] then
-                        map[y][x] = map[y-1][x]
+                if y > 1 and y < #grid then
+                    if grid[y-1][x] > -1 and grid[y+1][x] > -1 and grid[y-1][x] == grid[y+1][x] then
+                        grid[y][x] = grid[y-1][x]
                     end
                 end
             end
@@ -214,62 +214,62 @@ local function create_roads(map)
     end
 end
 
-local function reflect_grid(map)
-    local row = #map
-    local col = #map[1]
+local function reflect_grid(grid)
+    local row = #grid
+    local col = #grid[1]
 
     local re_col = col * 2
     local half = col
 
-    local re_map = {}
+    local re_grid = {}
     for i = 1, row do
-        re_map[i] = {}
+        re_grid[i] = {}
         for j = 1, re_col do
-            re_map[i][j] = 0
+            re_grid[i][j] = 0
         end
     end
 
     for i = 1, row do
         for j = 1, col do
-            re_map[i][half + j] = map[i][j]
-            re_map[i][half + 1 - j] = map[i][j]
+            re_grid[i][half + j] = grid[i][j]
+            re_grid[i][half + 1 - j] = grid[i][j]
         end
     end
 
-    return re_map
+    return re_grid
 end
 
-local function dfs(x, y, visited, map)
+local function dfs(x, y, visited, grid)
     visited[y][x] = 1;
 
     for i = -1, 1 do
         for j = -1, 1 do
-            if(y + i >= 1 and y + i <= #map and x + j >= 1 and x + j <= #map[1]) then
-                if(visited[y + i][x + j] == 0 and map[y + i][x + j] == -1) then
+            if(y + i >= 1 and y + i <= #grid and x + j >= 1 and x + j <= #grid[1]) then
+                if(visited[y + i][x + j] == 0 and grid[y + i][x + j] == -1) then
                     visited[y + i][x + j] = 1
-                    dfs(x + j, y + i, visited, map)
+                    dfs(x + j, y + i, visited, grid)
                 end
             end
         end
     end
 end
 
-local function clear_dead_ends(map, start_x, start_y)
+local function clear_dead_ends(grid, start_x, start_y)
     local visited = {}
-    for i = 1, #map do
+    for i = 1, #grid do
         visited[i] = {}
-        for j = 1, #map[1] do
+        for j = 1, #grid[1] do
             visited[i][j] = 0;
         end
     end
 
 
-    dfs(start_x, start_y, visited, map)
+    dfs(start_x, start_y, visited, grid)
 
-    for i = 1, #map do
-        for j = 1, #map[1] do
-            if map[i][j] == -1 and visited[i][j] == 0 then
-                map[i][j] = 1
+    for i = 1, #grid do
+        for j = 1, #grid[1] do
+            if grid[i][j] == -1 and visited[i][j] == 0 then
+                grid[i][j] = 1
             end
         end
     end
@@ -300,7 +300,8 @@ end
 local function make_map(grid, map, tileSize, world)
     local largeStructY = nil
     local largeStructX = nil
-    --ovde pozivam jednom funkciju mesto u petlji svaki put
+
+    --ovde pozivam jednom funkciju umesto u petlji svaki put
     local grassTexture = love.graphics.newImage("Assets/Vicolovka_grass.png")
     local pathTexture = love.graphics.newImage("Assets/Vicolovka_path.png")
     local ghostBox = love.graphics.newImage("Assets/Vicolovka_GhostBox_empty.png")
